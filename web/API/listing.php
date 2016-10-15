261df559007ca2a2ac05ffbbf3f0049b98203afd
Tony Forsythe <forsythetony@gmail.com>
2016-10-15 14:27:08 -0500

    "hi"

--- web/API/listing.php
+++ web/API/listing.php
@@ -53,7 +53,7 @@
 				
 				//	First we should do a check to make sure this book doesn't already
 				//	exist in our database
-				$check_isbn_sql = 	"SELECT * FROM Book WHERE ISBN = ? LIMIT 1";
+				$check_isbn_sql = 	"SELECT * FROM Book";
 				
 				if(!($check_isbn_stmt = $db_conn->prepare($check_isbn_sql))) {
 					set_generic_error_response( "There was an error preparing the statement ... " . $db_conn->error);
